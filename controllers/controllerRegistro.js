df7fcb0d6ea5e43e91b6c4cce1e5f958d92bd5ad
josetvasco <torovascojosedaniel@gmail.com>
2024-11-16 23:57:47 -0500

    Teminada la función de agregar los elementos del array al HTML

--- controllers/controllerRegistro.js
+++ controllers/controllerRegistro.js
@@ -1,6 +1,9 @@
 import { registros } from "../models/modelRegistro.js";
 
+document.addEventListener('DOMContentLoaded', imprimirHTML)
+
 const formulario = document.querySelector('#formularioRegistro');
+const infoRegistro = document.querySelector('#informacionRegistro');
 
 const btnGuardar = document.querySelector('#btnGuardar');
 
@@ -17,14 +20,14 @@ btnGuardar.addEventListener('click', validacionDeCampos);
 function validacionDeCampos(e) {
   e.preventDefault();
   
-  const tipoDocumento = document.querySelector('#tipoDocumento').value;
-  const documento = document.querySelector('#Documento').value;
-  const nombre = document.querySelector('#Nombre').value;
+  const tipoDocumento = document.querySelector('#tipoDocumento').value.toLowerCase();
+  const documento = document.querySelector('#Documento').value.toLowerCase();
+  const nombre = document.querySelector('#Nombre').value.toLowerCase();
   const radioSexo = document.querySelector('input[name=sexo]:checked');
-  const apellido = document.querySelector('#Apellido').value;
-  const telefono = document.querySelector('#Telefono').value;
-  const correo = document.querySelector('#Correo').value;
-  const roll = document.querySelector('#Rol').value;
+  const apellido = document.querySelector('#Apellido').value.toLowerCase();
+  const telefono = document.querySelector('#Telefono').value.toLowerCase();
+  const correo = document.querySelector('#Correo').value.toLowerCase();
+  const roll = document.querySelector('#Rol').value.toLowerCase();
   const user = document.querySelector('#Usuario').value;
   const password = document.querySelector('#Contrasena').value;
   const confirmar = document.querySelector('#Confirmar').value;
@@ -82,7 +85,7 @@ function validacionDuplicados(tipoDocumento, documento, nombre, radioSexo, apell
     tipoDocumento,
     documento,
     nombre,
-    radioSexo: radioSexo.value,
+    radioSexo, 
     apellido,
     telefono,
     correo,
@@ -102,6 +105,47 @@ function agregarUsuario(usuarioObj) {
     text: "El elemento fue guardado exitosamente!",
     icon: "success"
   });
+  
+  imprimirHTML();
+}
+
+function imprimirHTML() {
+  registros.forEach( registros => {
+    
+    const {tipoDocumento,documento, nombre, radioSexo, apellido, telefono, correo, roll, user, password} = registros;
+    
+    const tr = document.createElement('TR');
+    const tdDocumento = document.createElement('TD');
+    tdDocumento.innerText = `${tipoDocumento.toUpperCase()} ${documento}`;
+    const tdNombre = document.createElement('TD');
+    tdNombre.innerText = `${nombre.charAt(0).toUpperCase() + nombre.slice(1)} ${apellido.charAt(0).toUpperCase() + apellido .slice(1)} `;
+    const tdTelefono = document.createElement('TD');
+    tdTelefono.innerText = telefono;
+    const tdRol = document.createElement('TD');
+    tdRol.innerText = roll;
+    const tdCorreo = document.createElement('TD');
+    tdCorreo.innerText = correo;
+    const tdUsuario = document.createElement('TD');
+    tdUsuario.innerText = user;
+    const tdAcciones = document.createElement('TD');
+    const imgEliminar = document.createElement('IMG');
+    imgEliminar.src = '../img/delete.png';
+    imgEliminar.classList.add('delete');
+    tdAcciones.appendChild(imgEliminar);
+    imgEliminar.setAttribute('data-documento', documento);
+    const imgEditar = document.createElement('IMG');
+    imgEditar.src = '../img/editar.png';
+    imgEditar.classList.add('editar');
+    tdAcciones.appendChild(imgEditar);
+    imgEditar.setAttribute('data-documento', documento);
+    tr.append(tdDocumento, tdNombre, tdTelefono, tdRol, tdCorreo, tdUsuario, tdAcciones);
+
+    infoRegistro.append(tr);
+  });
+
+  sincronizarLocalStorage();
+}
+
+function sincronizarLocalStorage() {
   localStorage.setItem( 'registros', JSON.stringify(registros) );
-  console.log(registros);
 }
