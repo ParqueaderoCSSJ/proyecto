bf0e423be29967ee9d9fb61ca37a5a5f037c4d13
josetvasco <torovascojosedaniel@gmail.com>
2024-11-16 23:01:02 -0500

    Se agrega el objeto al array

--- controllers/controllerRegistro.js
+++ controllers/controllerRegistro.js
@@ -1,8 +1,18 @@
 import { registros } from "../models/modelRegistro.js";
 
+const formulario = document.querySelector('#formularioRegistro');
+
 const btnGuardar = document.querySelector('#btnGuardar');
 
-btnGuardar.addEventListener('click', validacionDeCampos)
+// agregarUsuario(usuarioObj);
+//         Swal.fire({
+//           title: "Éxito!",
+//           text: "El elemento fue guardado exitosamente!",
+//           icon: "success"
+//         });
+//         formulario.reset();
+
+btnGuardar.addEventListener('click', validacionDeCampos);
 
 function validacionDeCampos(e) {
   e.preventDefault();
@@ -14,29 +24,14 @@ function validacionDeCampos(e) {
   const apellido = document.querySelector('#Apellido').value;
   const telefono = document.querySelector('#Telefono').value;
   const correo = document.querySelector('#Correo').value;
-  const rol = document.querySelector('#Rol').value;
-  const cargo = document.querySelector('#cargo').value;
-  const usuario = document.querySelector('#Usuario').value;
-  const contrasena = document.querySelector('#Contrasena').value;
+  const roll = document.querySelector('#Rol').value;
+  const user = document.querySelector('#Usuario').value;
+  const password = document.querySelector('#Contrasena').value;
   const confirmar = document.querySelector('#Confirmar').value;
   
-  if(tipoDocumento !== '' && documento !== '' && nombre !== '' && radioSexo !== null && apellido !== '' && telefono !== '' && correo !== '' && rol !== '' && cargo !== '' && usuario !== '' && contrasena !== '' && confirmar !== '') {
-    if( contrasena === confirmar ) {
-      const usuarioObj = {
-        tipoDocumento,
-        documento,
-        nombre,
-        radioSexo,
-        apellido,
-        telefono,
-        correo,
-        rol,
-        cargo,
-        usuario,
-        contrasena,
-        confirmar
-      }
-      agregarUsuario(usuarioObj);
+  if(tipoDocumento !== '' && documento !== '' && nombre !== '' && radioSexo !== null && apellido !== '' && telefono !== '' && correo !== '' && roll !== '' && user !== '' && password !== '' && confirmar !== '') {
+    if( password === confirmar ) {
+      validacionDuplicados( tipoDocumento, documento, nombre, radioSexo.value, apellido, telefono, correo, roll, user, password);  
     } else {
       Swal.fire({
         title: "Error!",
@@ -51,5 +46,62 @@ function validacionDeCampos(e) {
       icon: "error"
     });
   }
+}
 
+function validacionDuplicados(tipoDocumento, documento, nombre, radioSexo, apellido, telefono, correo, roll, user, password) {
+  const encontradoDocumento = registros.some( usuario => usuario.documento === documento );
+  const encontradoUser = registros.some( usuario => usuario.user === user );
+  const encontradoEmail = registros.some( usuario => usuario.correo === correo );
+
+  if( encontradoDocumento ) {
+    Swal.fire({
+      title: "Error!",
+      text: "Documento de identidad ya existe",
+      icon: "error"
+    });
+    return;
+  }
+  if( encontradoUser ) {
+    Swal.fire({
+      title: "Error!",
+      text: "Usuario ya existe",
+      icon: "error"
+    });
+    return;
+  }
+  if( encontradoEmail ) {
+    Swal.fire({
+      title: "Error!",
+      text: "Email ya existe",
+      icon: "error"
+    });
+    return;
+  }
+
+  const usuarioObj = {
+    tipoDocumento,
+    documento,
+    nombre,
+    radioSexo: radioSexo.value,
+    apellido,
+    telefono,
+    correo,
+    roll,
+    user,
+    password
+  }
+
+  agregarUsuario(usuarioObj);
+}
+
+function agregarUsuario(usuarioObj) {
+  registros.push(usuarioObj);
+  formulario.reset();
+  Swal.fire({
+    title: "Éxito!",
+    text: "El elemento fue guardado exitosamente!",
+    icon: "success"
+  });
+  localStorage.setItem( 'registros', JSON.stringify(registros) );
+  console.log(registros);
 }
