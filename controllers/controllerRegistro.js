dee4849ab9935fd5a3849786e1e5fd3bed8d305b
josetvasco <torovascojosedaniel@gmail.com>
2024-11-18 19:45:50 -0500

    Terminada la parte del fromulario con almacenamiento en el LocalStorage y la validación de datos

--- controllers/controllerRegistro.js
+++ controllers/controllerRegistro.js
@@ -1,21 +1,10 @@
 import { registros } from "../models/modelRegistro.js";
 
-document.addEventListener('DOMContentLoaded', imprimirHTML)
-
 const formulario = document.querySelector('#formularioRegistro');
-const infoRegistro = document.querySelector('#informacionRegistro');
 
 const btnGuardar = document.querySelector('#btnGuardar');
 
-// agregarUsuario(usuarioObj);
-//         Swal.fire({
-//           title: "Éxito!",
-//           text: "El elemento fue guardado exitosamente!",
-//           icon: "success"
-//         });
-//         formulario.reset();
-
-btnGuardar.addEventListener('click', validacionDeCampos);
+btnGuardar.addEventListener('click', validacionDeCampos); 
 
 function validacionDeCampos(e) {
   e.preventDefault();
@@ -106,43 +95,6 @@ function agregarUsuario(usuarioObj) {
     icon: "success"
   });
   
-  imprimirHTML();
-}
-
-function imprimirHTML() {
-  registros.forEach( registros => {
-    
-    const {tipoDocumento,documento, nombre, radioSexo, apellido, telefono, correo, roll, user, password} = registros;
-    
-    const tr = document.createElement('TR');
-    const tdDocumento = document.createElement('TD');
-    tdDocumento.innerText = `${tipoDocumento.toUpperCase()} ${documento}`;
-    const tdNombre = document.createElement('TD');
-    tdNombre.innerText = `${nombre.charAt(0).toUpperCase() + nombre.slice(1)} ${apellido.charAt(0).toUpperCase() + apellido .slice(1)} `;
-    const tdTelefono = document.createElement('TD');
-    tdTelefono.innerText = telefono;
-    const tdRol = document.createElement('TD');
-    tdRol.innerText = roll;
-    const tdCorreo = document.createElement('TD');
-    tdCorreo.innerText = correo;
-    const tdUsuario = document.createElement('TD');
-    tdUsuario.innerText = user;
-    const tdAcciones = document.createElement('TD');
-    const imgEliminar = document.createElement('IMG');
-    imgEliminar.src = '../img/delete.png';
-    imgEliminar.classList.add('delete');
-    tdAcciones.appendChild(imgEliminar);
-    imgEliminar.setAttribute('data-documento', documento);
-    const imgEditar = document.createElement('IMG');
-    imgEditar.src = '../img/editar.png';
-    imgEditar.classList.add('editar');
-    tdAcciones.appendChild(imgEditar);
-    imgEditar.setAttribute('data-documento', documento);
-    tr.append(tdDocumento, tdNombre, tdTelefono, tdRol, tdCorreo, tdUsuario, tdAcciones);
-
-    infoRegistro.append(tr);
-  });
-
   sincronizarLocalStorage();
 }
 
