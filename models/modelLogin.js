0385af9c08d0d7e45c021ac49117aaca7493b8bf
josetvasco <torovascojosedaniel@gmail.com>
2024-11-19 20:43:58 -0500

    Terminada las correciones en el login

--- models/modelLogin.js
+++ models/modelLogin.js
@@ -1,8 +1,8 @@
 let usuarios = [
     {user:"sebas", password:"1234", roll:"admin", nombre: "Sebastián", apellido: "Vasquez", correo: "sebas@correo.com", documento: "1", radioSexo: "Hombre", telefono: "11111", tipoDocumento: "cc" },
-    {user:"johan", password:"1234", roll:"admin"},
-    {user:"sebastian", password:"1234", roll:"admin"},
-    {user:"jose", password:"1234", roll:"admin"}
+    {user:"johan", password:"1234", roll:"admin", nombre: "Sebastián", apellido: "Vasquez", correo: "sebas@correo.com", documento: "1", radioSexo: "Hombre", telefono: "11111", tipoDocumento: "cc" },
+    {user:"sebastian", password:"1234", roll:"admin", nombre: "Sebastián", apellido: "Vasquez", correo: "sebas@correo.com", documento: "1", radioSexo: "Hombre", telefono: "11111", tipoDocumento: "cc" },
+    {user:"jose", password:"1234", roll:"admin", nombre: "Sebastián", apellido: "Vasquez", correo: "sebas@correo.com", documento: "1", radioSexo: "Hombre", telefono: "11111", tipoDocumento: "cc" },
 ];
 
 document.addEventListener('DOMContentLoaded', functionArray)
