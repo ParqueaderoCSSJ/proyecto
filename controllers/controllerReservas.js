036917b4209d45e6129139649d94edf2d42c9b9b
Josanva777 <johansanchezvalencia@gmail.com>
2024-11-22 12:51:30 -0500

    reservas admin finalizado funcional

---
+++ controllers/controllerReservas.js
@@ -0,0 +1,78 @@
+import { Registros} from "../models/modelReservas.js";
+
+export function registrarRegistros() {
+    const placa = document.getElementById('lmatricula').value;
+    const diaEntrada = document.getElementById('ldia_entrada').value;
+    const diaSalida = document.getElementById('ldia_salida').value;
+    const tarifa = document.querySelector('input[name="Tarifa"]:checked').value;
+
+    if (!placa || !diaEntrada || !diaSalida || !tarifa) {
+        alert("Por favor, completa todos los campos.");
+        return;
+    }
+
+    const fechaEntrada = new Date(diaEntrada);
+    const fechaSalida = new Date(diaSalida);
+    const totalDias = Math.ceil((fechaSalida - fechaEntrada) / (1000 * 60 * 60 * 24));
+
+    const newRegistro = {
+        placa: placa,
+        tarifa: tarifa,
+        diaEntrada: diaEntrada,
+        diaSalida: diaSalida,
+        totalDias: isNaN(totalDias) || totalDias < 0 ? 0 : totalDias
+    };
+
+    Registros.push(newRegistro);
+    console.log(newRegistro);
+    crearRegistroTiempo(newRegistro);
+}
+
+function crearRegistroTiempo(newRegistro) {
+    const registro = document.createElement('tr');
+
+    const placa = document.createElement('td');
+    placa.textContent = newRegistro.placa;
+
+    const tarifa = document.createElement('td');
+    tarifa.textContent = newRegistro.tarifa;
+
+    
+    const diaEntrada = document.createElement('td');
+    diaEntrada.textContent = formatDate(newRegistro.diaEntrada);
+
+    const diaSalida = document.createElement('td');
+    diaSalida.textContent = formatDate(newRegistro.diaSalida);
+
+    const totalDias = document.createElement('td');
+    totalDias.textContent = newRegistro.totalDias;
+
+    const eliminar = document.createElement('td');
+    const imgEliminar = document.createElement('img');
+    imgEliminar.src = '../img/delete.png';
+    imgEliminar.alt = 'Eliminar';
+    imgEliminar.classList.add('delete');
+    imgEliminar.style.cursor = 'pointer';
+    imgEliminar.addEventListener('click', () => eliminarRegistro(registro, newRegistro));
+    eliminar.appendChild(imgEliminar);
+
+    registro.append(placa, tarifa, diaEntrada, diaSalida, totalDias, eliminar);
+
+    document.getElementById('registros').appendChild(registro);
+}
+
+
+function formatDate(dateString) {
+    const date = new Date(dateString);
+    return date.toLocaleString('es-ES', {
+        dateStyle: 'short',
+        timeStyle: 'short',
+    }); 
+}
+
+
+function eliminarRegistro(fila) {
+    fila.remove();
+}
+
+document.getElementById('btnRegistrar').addEventListener('click', registrarRegistros);
